4daa496c94f3141e922eefc703c61970c8e654f3
Ali Seifi <seifiali84@gmail.com>
2023-02-13 19:25:03 +0330

    TRX class was Defined.

--- Block.cs
+++ Block.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -10,7 +11,7 @@ namespace Pcoin
     {
         public int id { get; set; }
         public DateTime TimeStamp { get; set; }
-        public double BlockReward { get; set; }
+        public double Reward { get; set; } // for start its 50(Pcoin)  Like BTC :D
         public List<TRX> TRXs { get; set; } // No limit for trx count
         public int Dificulty { get; set; } // Dificulty level of network
         public string PreviousHash { get; set; }
---
+++ TRX.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Pcoin
+{
+    public class TRX
+    {
+        public int id { get; set; }
+        public string Sender { get; set; } // public key of transaction sender
+        public string Receiver { get; set; } // public key of transaction Receiver
+        public double Amount { get; set; } // Amount of transaction
+        public DateTime TimeStamp { get; set; } // Time of Transaction
+        public double fee { get; set; } // Transaction fee
+        public string feeGetter { get; set; } // This Property Name should be changed with better word. 
+    }
+}
